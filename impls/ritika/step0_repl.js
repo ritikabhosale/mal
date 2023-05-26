f18f545c4a75d4189f9dc35350e1423925d9a601
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2023-05-26 11:06:03 +0530

    Add step 0 that takes input from command line and prints it

---
+++ impls/ritika/step0_repl.js
@@ -0,0 +1,24 @@
+const { stdin, stdout } = require("process");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const READ = (arg) => arg;
+
+const EVAL = (arg) => arg;
+
+const PRINT = (arg) => arg;
+
+const rep = (arg) => PRINT(EVAL(READ(arg)));
+
+const main = () => {
+  rl.question("user> ", (input) => {
+    console.log(rep(input));
+    main();
+  });
+};
+
+main();
