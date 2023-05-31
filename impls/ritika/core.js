30a745be114469efc34b5bc6d13ec73007b3a328
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2023-05-31 16:03:04 +0530

    Add the feature of deep equal

--- impls/ritika/core.js
+++ impls/ritika/core.js
@@ -1,6 +1,25 @@
 const { MalList, MalNil } = require("./types");
 
-const areEqual = (args) => args.every((item) => item === args[0]);
+const areAllEqual = (args) => {
+  return args.every((item) => deepEquals(item, args[0]));
+};
+
+const deepEquals = (a, b) => {
+  if (typeof a !== typeof b) return false;
+
+  if (typeof a === "number" && typeof b === "number") return a === b;
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+
+    if (a.length === 0 && b.length === 0) return true;
+
+    if (!deepEquals(a[0], b[0])) return false;
+    return deepEquals(a.slice(1), b.slice(1));
+  }
+
+  return true;
+};
 
 const isInDescendingOrder = (items) => {
   const list = [...items];
@@ -31,7 +50,7 @@ const ns = {
   "-": (...args) => args.reduce((a, b) => a - b),
   "*": (...args) => args.reduce((a, b) => a * b),
   "/": (...args) => args.reduce((a, b) => a / b),
-  "=": (...args) => areEqual(args),
+  "=": (...args) => areAllEqual(args),
   ">=": (...args) => isInDescendingOrder(args),
   "<=": (...args) => isInAscendingOrder(args),
   ">": (...args) => isNextGreaterThanCurrent(args),
